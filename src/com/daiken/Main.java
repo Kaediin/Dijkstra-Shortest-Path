e0a374e065c6dfb28153f4f31cc1dddfa4bf937c
kaediin <skaedin@gmail.com>
2021-05-27 14:36:11 +0200

    Added arguments to Main.java

--- src/com/daiken/Main.java
+++ src/com/daiken/Main.java
@@ -11,6 +11,8 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        // Create a list with flights
+        // We define the name, cost and % chance that the bagage is lost
         List<Vlucht> vluchten = new ArrayList<>(Arrays.asList(
                 new Vlucht("Parijs", 200, 0.18),
                 new Vlucht("Barcelona", 150, 0.05),
@@ -18,12 +20,15 @@ public class Main {
                 new Vlucht("Berlijn", 340, 0.03),
                 new Vlucht("Milaan" , 160, 0.02)
         ));
+        // Create the links between the flights. This is scalable as it is a list
         vluchten.get(0).setConnections(Arrays.asList(vluchten.get(1), vluchten.get(4)));
         vluchten.get(1).setConnections(Arrays.asList(vluchten.get(4), vluchten.get(3)));
         vluchten.get(2).setConnections(Arrays.asList(vluchten.get(1), vluchten.get(3)));
         vluchten.get(3).setConnections(Arrays.asList(vluchten.get(2), vluchten.get(0)));
         vluchten.get(4).setConnections(Arrays.asList(vluchten.get(3), vluchten.get(2)));
 
+        // Create a list with car-routes
+        // We define the highway-name and kilometeres
         List<Rit> ritten = new ArrayList<>(Arrays.asList(
                 new Rit("A1", 80),
                 new Rit("A2", 60),
@@ -31,12 +36,15 @@ public class Main {
                 new Rit("A10", 120),
                 new Rit("A27", 50)
         ));
+        // Create the links between the cars. This is scalable as it is a list
         ritten.get(0).setConnections(Arrays.asList(ritten.get(2), ritten.get(3)));
         ritten.get(1).setConnections(Arrays.asList(ritten.get(2), ritten.get(4)));
         ritten.get(2).setConnections(Arrays.asList(ritten.get(1), ritten.get(3)));
         ritten.get(3).setConnections(Arrays.asList(ritten.get(2), ritten.get(0)));
         ritten.get(4).setConnections(Arrays.asList(ritten.get(0), ritten.get(1)));
 
+        // Create a list with train-routes
+        // We define the city-name and minutes it takes
         List<Treinreis> treinreizen = new ArrayList<>(Arrays.asList(
                 new Treinreis("Almere", 25),
                 new Treinreis("Utrecht", 15),
@@ -44,12 +52,14 @@ public class Main {
                 new Treinreis("Arhnem", 30),
                 new Treinreis("Den Haag", 60)
         ));
+        // Create the links between the trains. This is scalable as it is a list
         treinreizen.get(0).setConnections(Arrays.asList(treinreizen.get(1), treinreizen.get(4)));
         treinreizen.get(1).setConnections(Arrays.asList(treinreizen.get(4), treinreizen.get(3)));
         treinreizen.get(2).setConnections(Arrays.asList(treinreizen.get(1), treinreizen.get(3)));
         treinreizen.get(3).setConnections(Arrays.asList(treinreizen.get(2), treinreizen.get(0)));
         treinreizen.get(4).setConnections(Arrays.asList(treinreizen.get(3), treinreizen.get(2)));
 
+        // Create a reis object for each vehicletype. We also compute and print the output
         Reis reisVluchten = new Reis(vluchten.get(4), vluchten.get(0));
         reisVluchten.compute();
         Reis reisAutoritten = new Reis(ritten.get(0), ritten.get(3));
