bc976e4ba45535d5705b139423f7e75f37343b01
kaediin <skaedin@gmail.com>
2021-05-27 15:00:29 +0200

    Added arguments to Rit.java

--- src/com/daiken/models/Rit.java
+++ src/com/daiken/models/Rit.java
@@ -2,22 +2,23 @@ package com.daiken.models;
 
 public class Rit extends Stap{
 
+    // private attribute kilometers
     private double kilometers;
 
+    // Constructor
     public Rit(String name, double kilometers) {
         super(name);
         this.kilometers = kilometers;
+        // for this object we use the kilometers as the weight
         this.setGewicht(kilometers);
     }
 
+    // Getters and setters
     public double getKilometers() {
         return kilometers;
     }
 
     public void setKilometers(double kilometers) {
         this.kilometers = kilometers;
-    }
-
-    public Rit() {
     }
 }
