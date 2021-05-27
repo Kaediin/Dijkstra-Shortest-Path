4d508ebe29e337742621d776a7ce2ec4d0ba9620
kaediin <skaedin@gmail.com>
2021-05-27 15:05:54 +0200

    Added arguments to Treinreis.java

--- src/com/daiken/models/Treinreis.java
+++ src/com/daiken/models/Treinreis.java
@@ -2,18 +2,18 @@ package com.daiken.models;
 
 public class Treinreis extends Stap {
 
+    // Object attibutes
     private int minuten;
 
+    // Constructor
     public Treinreis(String name, int minuten) {
         super(name);
         this.minuten = minuten;
+        // We use the minutes as weights
         this.setGewicht(minuten);
     }
 
-    public Treinreis(int minuten) {
-        this.minuten = minuten;
-    }
-
+    // Getters and setters
     public int getMinuten() {
         return minuten;
     }
