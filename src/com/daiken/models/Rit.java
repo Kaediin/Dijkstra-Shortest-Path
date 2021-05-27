a434f4fb3fbb7675071fac1daec96df5e443922d
kaediin <skaedin@gmail.com>
2021-05-27 13:49:44 +0200

    Added Rit.java object

---
+++ src/com/daiken/models/Rit.java
@@ -0,0 +1,23 @@
+package com.daiken.models;
+
+public class Rit extends Stap{
+
+    private double kilometers;
+
+    public Rit(String name, double kilometers) {
+        super(name);
+        this.kilometers = kilometers;
+        gewicht = kilometers;
+    }
+
+    public double getKilometers() {
+        return kilometers;
+    }
+
+    public void setKilometers(double kilometers) {
+        this.kilometers = kilometers;
+    }
+
+    public Rit() {
+    }
+}
